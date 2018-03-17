15262eae5ae225ee252e037f6c1ad3736cff116b
Douglas Monteiro <douglasmonteiro@icloud.com>
2018-03-17 14:31:01 -0300

    create foto component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FotoModule } from './components/foto/foto.module';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FotoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
